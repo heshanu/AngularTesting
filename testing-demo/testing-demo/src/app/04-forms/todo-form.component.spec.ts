9c98c586bbfe8d8d29eaf9813ede6310fdc7e31b
heshanu <heshanu97@gmail.com>
2024-01-12 19:34:03 +0530

    updated

--- testing-demo/testing-demo/src/app/04-forms/todo-form.component.spec.ts
+++ testing-demo/testing-demo/src/app/04-forms/todo-form.component.spec.ts
@@ -1,15 +1,25 @@
+import {FormBuilder} from '@angular/forms';
 import { TodoFormComponent } from './todo-form.component'; 
 
 describe('TodoFormComponent', () => {
   var component: TodoFormComponent; 
 
   beforeEach(() => {
-
+    component=new TodoFormComponent();
   });
 
-  it('', () => {
+  it('should create a form with 2 controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+    expect(component.form.contains('email')).toBeTruthy();
   });
 
-  it('', () => {
+  it('should validate name input', () => {
+    expect(component.form.controls['name'].valid).toBeFalsy();
   });
+
+  it('should validate email input', () => {
+    expect(component.form.controls['email'].valid).toBeFalsy();
+  });
+  
+
 });
