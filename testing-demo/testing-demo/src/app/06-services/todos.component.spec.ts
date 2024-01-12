ed4d39594bed3a2c0281f1464c3e180f787aac71
heshanu <heshanu97@gmail.com>
2024-01-12 19:48:42 +0530

    implemented unit case for todo service

--- testing-demo/testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/testing-demo/src/app/06-services/todos.component.spec.ts
@@ -1,12 +1,33 @@
 import { TodosComponent } from './todos.component'; 
 import { TodoService } from './todo.service'; 
+//import { Http } from '@angular/http';
+//import  {RxJs}
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
+  let service: TodoService;
 
   beforeEach(() => {
+    service = new TodoService(null);
+    component = new TodosComponent(service);
+
   });
 
-  it('', () => {
+  it('should set todo item returned from service', () => {
+    
+    let todos = [
+      { id: 1, name: 'test1' },
+      { id: 2, name: 'test2' },
+      { id: 3, name: 'test3' },
+    ];
+
+    spyOn(service, 'getTodos').and.callFake(() => {
+      return Observable.from([todos]);
+
+      component.ngOnInit();
+      //expect(component.todos.length).toBe(3);
+      // expects(component.todos.length).toBeGreaterThan(0);
+      //expects(component.todos).toBe(todos);
+    });
   });
 });
